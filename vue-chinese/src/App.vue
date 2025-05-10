43d0b7bd2b8fb555afb8d6d7e38542d23ce3261d
Huntrese <bananananaaa6@gmail.com>
2025-05-10 18:06:28 +0300

    add reset css + start work on themeing

--- vue-chinese/src/App.vue
+++ vue-chinese/src/App.vue
@@ -2,22 +2,20 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 
-import { computed } from 'vue'
 
-const isDarkMode = computed(() => window.matchMedia('(prefers-color-scheme: dark)').matches)
-
-document.documentElement.classList.toggle('dark', isDarkMode.value)
 </script>
 
 
 
 
 <template>
-  <div class="flex flex-col items-center justify-start pt-2 gap-8" :class="{ 'dark': isDarkMode }">
-    <header class="flex flex-col items-center justify-start ">
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
-
-
+  <div class="theme-controller flex flex-col items-center justify-start px-8 py-2 gap-8 min-h-full">
+    <header class="flex flex-col items-center w-full">
+      <div class="flex flex-row items-center justify-between mt-4 space-x-4 text-2xl">
+        <div></div>
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
+        <input type="checkbox" value="caramellatte" class="toggle theme-controller" />
+      </div>
       <nav class="flex flex-row items-center justify-center mt-4 space-x-4 text-xl">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/stuff">stuff</RouterLink>
